b335da0a62f514100d73dbbc72e68f650e752c5b
khushboo149 <71179638+khushboo149@users.noreply.github.com>
2021-01-22 22:52:55 +0530

    Add files via upload

--- mango.js
+++ mango.js
@@ -1,22 +1,30 @@
-class Mango{
-    constructor(x,y,diametre){
-        var options={
-            isStatic:true,
-            'restituion':0.5,
-            'friction': 1.0,
-        }
-        this.body=Bodies.circle(x,y,diametre,options);
-        this.diametre=diametre;
-        this.image=loadImage("images/img/mango.png");
-        World.add(world,this.body);
-    }
-    display(){
-        push();
-        translate(this.body.position.x, this.body.position.y);
+class mango{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution :0,
+            friction :1,
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("images/img/mango.png")
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
 
-        var mango=createSprite(0,0,this.diametre,this.diametre);
-        mango.addImage(this.image);
-        mango.scale=0.22;
-        pop();
-    }
+	display()
+	{
+		var mangoPos=this.body.position;	
+		push()
+		translate(mangoPos.x, mangoPos.y);
+		// rectMode(CENTER);
+		rotate(this.body.angle)
+		fill(255,0,255)
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2)
+		pop()
+ }
 }
--- stone.js
+++ stone.js
@@ -1,22 +1,33 @@
-class Stone{
-    constructor(x,y,diametre){
-        var options= {
-            isStatic: false,
-            restitution:0,
-            friction:1,
-            density:1.2
-        }
-        this.body = Bodies.circle(x,y,diametre,options);
-        this.diametre=diametre;
-        this.image=loadImage("images/img/stone.png");
-        World.add(world,this.body);
-    }
-    display(){
-        push();
-        translate(this.body.position.x,this.body.position.y);
-        var stone=createSprite(0,0,this.diametre,this.diametre);
-        stone.addImage(this.image);
-        stone.scale=0.3;
-        pop();
-    }
+class stone
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0,
+			friction:1,
+			density:1.2
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("images/img/stone.png");
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var stonepos=this.body.position;		
+			push()
+			translate(stonepos.x, stonepos.y);
+			
+			fill(255,0,255)
+			imageMode(CENTER);
+			ellipseMode(RADIUS)
+			image(this.image, 0,0,this.r*2, this.r*2)
+			pop()
+			
+	}
+
 }
